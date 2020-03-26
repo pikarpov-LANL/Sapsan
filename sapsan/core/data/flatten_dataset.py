a31d1e869bc2e786f30d5a7a8018fba28262b7c3
IceKhan13 <iskandar.sitdikov@gmail.com>
2020-03-25 22:28:29 -0700

    Fix: remove ordered dict

--- sapsan/core/data/flatten_dataset.py
+++ sapsan/core/data/flatten_dataset.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict, OrderedDict, Optional
+from typing import List, Tuple, Dict, Optional
 import numpy as np
 import h5py
 from skimage.util.shape import view_as_blocks
