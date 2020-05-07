0293102d8cd82f5a03784f467d81ae909adbdb71
pikarpov <plkarpov@ucsc.edu>
2020-05-06 17:34:11 -0700

    Fixed the tests as per the name changes

--- tests/sapsan/estimator/krr_estimator_test.py
+++ tests/sapsan/estimator/krr_estimator_test.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import unittest
 
-from sapsan.lib.estimator import KrrEstimator, KrrEstimatorConfiguration
+from sapsan.lib.estimator import KRR, KRRConfig
 
 
 class TestKrrEstimator(unittest.TestCase):
@@ -12,11 +12,11 @@ class TestKrrEstimator(unittest.TestCase):
         os.mkdir(self.resources_path)
 
     def test_save_and_load(self):
-        estimator = KrrEstimator(
-            KrrEstimatorConfiguration(gamma=0.1, alpha=0.2)
+        estimator = KRR(
+            KRRConfig(gamma=0.1, alpha=0.2)
         )
         estimator.save(self.resources_path)
-        loaded_estimator = KrrEstimator.load(self.resources_path)
+        loaded_estimator = KRR.load(self.resources_path)
         self.assertEqual(estimator.config.gamma, loaded_estimator.config.gamma)
         self.assertEqual(estimator.config.alpha, loaded_estimator.config.alpha)
 
