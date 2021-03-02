9bc3b4dfe91206703c9d41deaf433f09f029f7eb
Platon I Karpov <57731529+pikarpov-LANL@users.noreply.github.com>
2021-03-02 13:49:14 -0700

    Docker, Requirements, CLI templates (#86)
    * Updated notebook and tornado version reqs
    
    * Updated reqs
    
    * Added comments to Docker
    
    * Added jupyter 1.0.0 to reqs
    
    * Updated estimator template + CLI
    
    Co-authored-by: Platon I. Karpov <plkarpov@ucsc.edu>

--- sapsan/core/cli/templates/docker.py
+++ sapsan/core/cli/templates/docker.py
@@ -1,16 +1,23 @@
 TEMPLATE = """
-FROM python:3.7.7-slim
+FROM python:3.8.5-slim
+
+# which port to expose
+EXPOSE 7777
+
+ENV GIT_PYTHON_REFRESH=quiet
+RUN pip install -U pip
+RUN pip install sapsan
 
 WORKDIR /app/
+COPY ./workdir/ /app/
 
-COPY ./requirements.txt /app
+#install additional packages
+#COPY requirements.txt /app/
+#RUN pip install -r requirements.txt
 
-RUN pip install numpy
-RUN pip install -r /app/requirements.txt
 
-COPY ./ /app/
-
-CMD ["python", "{name}_runner.py"]
+# run your notebook!
+ENTRYPOINT ["jupyter", "notebook", "example.ipynb", "--port=7777", "--ip=0.0.0.0", "--allow-root", "--NotebookApp.token=''", "--NotebookApp.password=''", "--no-browser"] 
 """
 
 
---
+++ sapsan/core/cli/templates/estimator.py
@@ -0,0 +1,99 @@
+TEMPLATE = '''
+"""
+Estimator Template
+
+Please replace everything between triple quotes to create
+your custom estimator.
+"""
+import json
+import numpy as np
+
+import torch
+
+from sapsan.core.models import EstimatorConfig
+from sapsan.lib.estimator.cnn.pytorch_estimator import TorchEstimator
+
+
+class """AlgorithmNameModel"""(torch.nn.Module):
+    # input channels, output channels
+    def __init__(self, D_in, D_out):
+        super("""AlgorithmNameModel""", self).__init__()
+        
+        # define your layers
+        """
+        self.layer_1 = torch.nn.Linear(D_in*4, D_in*8)
+        self.layer_2 = torch.nn.Linear(D_in*8, D_out)
+        """
+
+    def forward(self, x): 
+
+        # set the layer order here
+        
+        """
+        l1 = self.layer_1(x)
+        output = self.layer_2(l1)
+        """
+
+        return output
+    
+    
+class """AlgorithmNameConfig"""(EstimatorConfig):
+    
+    # set defaults per your liking, add more parameters
+    def __init__(self,
+                 n_epochs: int = 1,
+                 batch_dim: int = 64,
+                 patience: int = 10,
+                 min_delta: float = 1e-5, 
+                 logdir: str = "./logs/",
+                 *args, **kwargs):
+        self.n_epochs = n_epochs
+        self.batch_dim = batch_dim
+        self.logdir = logdir
+        self.patience = patience
+        self.min_delta = min_delta
+        
+        #a few custom names for parameters to record in mlflow
+        self.parameters = {
+                        "model - n_epochs": self.n_epochs,
+                        "model - min_delta": self.min_delta,
+                        "model - patience": self.patience,
+                    }
+
+    @classmethod
+    def load(cls, path: str):
+        with open(path, 'r') as f:
+            cfg = json.load(f)
+            return cls(**cfg)
+
+    def to_dict(self):
+        return self.parameters    
+    
+    
+class """AlgorithmName"""(TorchEstimator):
+    def __init__(self, config: """AlgorithmNameConfig""", model=None):
+        super().__init__(config, model)
+        self.config = config
+        self.model = """AlgorithmNameModel"""(1, 1)
+        
+    def setup_model(self, n_input_channels, n_output_channels):
+        return """AlgorithmNameModel"""(n_input_channels, self.config.batch_dim ** 3 * n_output_channels)
+
+    def train(self, inputs, targets=None):
+
+        self.model = self.setup_model(inputs.shape[1], targets.shape[1])
+        optimizer = """ optimizer """
+        loss_func = """ loss finctions """
+        scheduler = """ scheduler """
+        
+        model = self.torch_train(inputs, targets, 
+                                 self.model, optimizer, loss_func, scheduler, self.config)
+                
+        return model
+
+'''
+
+
+def get_template(name: str):
+    return TEMPLATE.format(name=name.lower(),
+                           name_upper=name.capitalize())
