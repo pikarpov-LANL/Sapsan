bc4e7268385662ebb7651b7314f1c3b9f4421237
pikarpov-lanl <pkarpov@lanl.gov>
2021-08-26 23:58:22 -0600

    Data loader and Eval accept data without target
    	- useful when there is no ground truth to compare to
    	- will still output pdf, cdf, and spatial,
    	  without comparison metrics of course
    	- eval now output a dict of "pred_cube",
    	  with "target_cube" if such is provided

--- sapsan/lib/data/data_functions.py
+++ sapsan/lib/data/data_functions.py
@@ -7,41 +7,53 @@ from torch.utils.data import DataLoader, TensorDataset
 from sapsan.core.models import Dataset, DatasetPlugin
 import warnings
 
-def torch_splitter(x, y, 
+def torch_splitter(loaders, 
                    batch_num: int = 1,
                    train_fraction = None,
                    shuffle: bool = False):
 
-    if np.shape(x)[0]==1 and train_fraction!=None:
-        print('\nWARNING: your batch_num=1, hence the data cannot be split into train and valid (perhaps you only loaded 1 checkpoint). Setting valid = test data...\n')
-        train_fraction = None
+    if len(loaders)==1: 
+        x = loaders[0]
+        dataset=TensorDataset(from_numpy(x).float())
+        train_loader = DataLoader(dataset=TensorDataset(from_numpy(x).float()),
+                                  batch_size=batch_num,
+                                  shuffle=shuffle,
+                                  num_workers=4)
+        return OrderedDict({"train": train_loader})   
         
-    if train_fraction != None:            
-        x_train, x_valid, y_train, y_valid = train_test_split(x, y,
-                                                              train_size=train_fraction,
-                                                              shuffle=shuffle)
-    else:
-        x_train = x_valid = x
-        y_train = y_valid = y
+    else: 
+        x, y = loaders
+        
+        if np.shape(x)[0]==1 and train_fraction!=None:
+            print('\nWARNING: your batch_num=1, hence the data cannot be split into train and valid (perhaps you only loaded 1 checkpoint). Setting valid = test data...\n')
+            train_fraction = None
 
-    dataset=TensorDataset(from_numpy(x_train).float(),
-                                                    from_numpy(y_train).float())
-    train_loader = DataLoader(dataset=TensorDataset(from_numpy(x_train).float(),
-                                                    from_numpy(y_train).float()),
-                              batch_size=batch_num,
-                              shuffle=shuffle,
-                              num_workers=4)
+        if train_fraction != None:            
+            x_train, x_valid, y_train, y_valid = train_test_split(x, y,
+                                                                  train_size=train_fraction,
+                                                                  shuffle=shuffle)
+        else:
+            x_train = x_valid = x
+            y_train = y_valid = y
 
-    valid_loader = DataLoader(dataset=TensorDataset(from_numpy(x_valid).float(),
-                                                  from_numpy(y_valid).float()),
-                              batch_size=batch_num,
-                              shuffle=shuffle,
-                              num_workers=4)
-    print('Train data shapes: ', x_train.shape, y_train.shape)
-    print('Valid data shapes: ', x_valid.shape, y_valid.shape)
-    
-    return OrderedDict({"train": train_loader, "valid": valid_loader})    
-    
+        dataset=TensorDataset(from_numpy(x_train).float(),
+                              from_numpy(y_train).float())
+        train_loader = DataLoader(dataset=TensorDataset(from_numpy(x_train).float(),
+                                                        from_numpy(y_train).float()),
+                                  batch_size=batch_num,
+                                  shuffle=shuffle,
+                                  num_workers=4)
+
+        valid_loader = DataLoader(dataset=TensorDataset(from_numpy(x_valid).float(),
+                                                      from_numpy(y_valid).float()),
+                                  batch_size=batch_num,
+                                  shuffle=shuffle,
+                                  num_workers=4)
+        print('Train data shapes: ', x_train.shape, y_train.shape)
+        print('Valid data shapes: ', x_valid.shape, y_valid.shape)
+
+        return OrderedDict({"train": train_loader, "valid": valid_loader})    
+
     
 def flatten(data: np.ndarray):
     return data.reshape(data.shape[0], -1)
--- sapsan/lib/experiments/evaluate.py
+++ sapsan/lib/experiments/evaluate.py
@@ -4,6 +4,7 @@ evaluation_experiment = Evaluate(backend=tracking_backend,
                                  model=training_experiment.model,
                                  data_parameters = data_loader)
 
+#if target is given
 target_cube, pred_cube = evaluation_experiment.run()
 """
 
@@ -22,7 +23,7 @@ from sapsan.utils.plot import pdf_plot, cdf_plot, slice_plot, plot_params
 from sapsan.utils.shapes import combine_cubes, slice_of_cube
 
 class Evaluate(Experiment):
-    def __init__(self,                 
+    def __init__(self,
                  model: Estimator,
                  data_parameters,
                  backend = FakeBackend(),
@@ -37,20 +38,26 @@ class Evaluate(Experiment):
         self.batch_num = self.data_parameters.batch_num
         self.cmap = cmap
         self.axis = len(self.input_size)
+        self.targets_given = True
+        self.flat = flat
+        self.artifacts = []        
         
         if type(self.model.loaders) in [list, np.array]:
             self.inputs = self.model.loaders[0]
-            self.targets = self.model.loaders[1]
+            try: 
+                self.targets = self.model.loaders[1]
+            except: 
+                self.targets_given = False
+                print('Warning: no target given; only predicting...')
         else:
-            self.inputs, self.targets = iter(self.model.loaders['train']).next()
-            self.targets = self.targets.numpy()
+            try:
+                self.inputs, self.targets = iter(self.model.loaders['train']).next()
+                self.targets = self.targets.numpy()
+            except: 
+                self.inputs = iter(self.model.loaders['train']).next()[0]
+                self.targets_given = False
+                print('Warning: no target given; only predicting...')
         
-        self.flat = flat
-        if self.flat: self.n_output_channels = self.targets.shape[0] #flat arrays don't have batches
-        elif len(self.targets.shape)<(self.axis+2): self.n_output_channels = 1
-        else: self.n_output_channels = self.targets.shape[1]
-
-        self.artifacts = []
         
     def get_metrics(self) -> Dict[str, float]:
         if 'train' in self.model.metrics():
@@ -81,33 +88,29 @@ class Evaluate(Experiment):
         start = time.time()
         
         self.backend.start('evaluate', nested = True)
-        pred = self.model.predict(self.inputs, self.model.config)
-        
+        pred = self.model.predict(self.inputs, self.model.config)              
+                      
         end = time.time()
         runtime = end - start
         self.backend.log_metric("eval - runtime", runtime)
-        
-        mpl.rcParams.update(plot_params())
-        
-        fig = plt.figure(figsize=(12,6), dpi=60)
-        (ax1, ax2) = fig.subplots(1,2)
 
-        pdf = pdf_plot([pred, self.targets], names=['predict', 'target'], ax=ax1)
-        plt.savefig("pdf_plot.png")
-        self.artifacts.append("pdf_plot.png")
-
-        cdf = cdf_plot([pred, self.targets], names=['predict', 'target'], ax=ax2)
-        plt.savefig("cdf_plot.png")
-        self.artifacts.append("cdf_plot.png")                        
-
-        if self.flat: pred_slice, target_slice, pred_cube, target_cube = self.flatten(pred)
-        else: pred_slice, target_slice, pred_cube, target_cube = self.split_batch(pred)
-                
-        slices = slice_plot([pred_slice, target_slice], names=['predict', 'target'], cmap=self.cmap)
-        plt.savefig("slices_plot.png")
-        self.artifacts.append("slices_plot.png")        
+        #determine n_output_channels form prediction
+        if self.flat: self.n_output_channels = pred.shape[0] #flat arrays don't have batches
+        elif len(pred.shape)<(self.axis+2): self.n_output_channels = 1
+        else: self.n_output_channels = pred.shape[1]
         
-        self.experiment_metrics["eval - MSE Loss"] = np.square(np.subtract(target_cube, pred_cube)).mean()         
+        if self.targets_given: 
+            series = [pred, self.targets]
+            names = ['predict', 'target']
+        else: 
+            series = [pred]
+            names = ['predict']
+        
+        slices_cubes = self.analytic_plots(series, names)
+        
+        if self.targets_given:
+            self.experiment_metrics["eval - MSE Loss"] = np.square(np.subtract(slices_cubes['target_cube'], 
+                      slices_cubes['pred_cube'])).mean()         
 
         for metric, value in self.get_metrics().items():
             self.backend.log_metric(metric, value)
@@ -122,8 +125,12 @@ class Evaluate(Experiment):
         self._cleanup()
         
         print("eval - runtime: ", runtime)
+
+        cube_series = dict()
+        for key, value in slices_cubes.items():
+            if 'cube' in key: cube_series[key] = value
         
-        return target_cube, pred_cube
+        return cube_series
     
     
     def flatten(self, pred):
@@ -134,24 +141,69 @@ class Evaluate(Experiment):
             cube_shape = (self.n_output_channels, self.input_size[0], self.input_size[1])
             
         pred_cube = pred.reshape(cube_shape)
-        target_cube = self.targets.reshape(cube_shape)
-
         pred_slice = slice_of_cube(pred_cube)
-        target_slice = slice_of_cube(target_cube)
-
-        return pred_slice, target_slice, pred_cube, target_cube
+        slices_cubes['pred_slice'] = pred_slice
+        slices_cubes['pred_cube'] = pred_cube
+                      
+        if self.targets_given:              
+            target_cube = self.targets.reshape(cube_shape)
+            target_slice = slice_of_cube(target_cube)
+            slices_cubes['target_slice'] = target_slice
+            slices_cubes['target_cube'] = target_cube
+                      
+        return slices_cubes
     
     
     def split_batch(self, pred):
+        slices_cubes = dict()
         n_entries = self.inputs.shape[0]
         cube_shape = (n_entries, self.n_output_channels,
                       self.batch_size[0], self.batch_size[1], self.batch_size[2])
-        print(cube_shape)
-        pred_cube = pred.reshape(cube_shape)
-        target_cube = self.targets.reshape(cube_shape)
-
+        
+        pred_cube = pred.reshape(cube_shape)        
         pred_slice = slice_of_cube(combine_cubes(pred_cube,
                                                  self.input_size, self.batch_size))
-        target_slice = slice_of_cube(combine_cubes(target_cube,
-                                                   self.input_size, self.batch_size))
-        return pred_slice, target_slice, pred_cube, target_cube
+        slices_cubes['pred_slice'] = pred_slice
+        slices_cubes['pred_cube'] = pred_cube
+                      
+        if self.targets_given: 
+            target_cube = self.targets.reshape(cube_shape)
+            target_slice = slice_of_cube(combine_cubes(target_cube,
+                                         self.input_size, self.batch_size))
+            slices_cubes['target_slice'] = target_slice
+            slices_cubes['target_cube'] = target_cube
+                      
+        return slices_cubes
+                      
+                      
+    def analytic_plots(self, series, names):
+        mpl.rcParams.update(plot_params())
+                      
+        fig = plt.figure(figsize=(12,6), dpi=60)
+        (ax1, ax2) = fig.subplots(1,2)
+
+        pdf = pdf_plot(series, names=names, ax=ax1)
+        plt.savefig("pdf_plot.png")
+        self.artifacts.append("pdf_plot.png")
+
+        cdf = cdf_plot(series, names=names, ax=ax2)
+        plt.savefig("cdf_plot.png")
+        self.artifacts.append("cdf_plot.png")                        
+
+        pred = series[0]
+        if self.flat: slices_cubes = self.flatten(pred)
+        else: slices_cubes = self.split_batch(pred)
+        
+        slice_series = []
+        slice_names = []
+        for key, value in slices_cubes.items():
+            if 'slice' in key: 
+                slice_series.append(value)
+                slice_names.append(key)
+                      
+        slices = slice_plot(slice_series, names=slice_names, cmap=self.cmap)
+        plt.savefig("slices_plot.png")
+        self.artifacts.append("slices_plot.png")
+                      
+        return slices_cubes
+        
