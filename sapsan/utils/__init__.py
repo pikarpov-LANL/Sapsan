66dfcd28d3f049e4aa28ded3048093f6242ffbff
pikarpov-lanl <pkarpov@lanl.gov>
2021-09-22 11:54:08 -0600

    Added all plot and filters to __init__

--- sapsan/utils/__init__.py
+++ sapsan/utils/__init__.py
@@ -1,1 +1,3 @@
 from .physics import PowerSpectrum, GradientModel, DynamicSmagorinskyModel, picae_func
+from .plot import plot_params, pdf_plot, cdf_plot, slice_plot, line_plot, model_graph, log_plot
+from .filters import spectral, box, gaussian
