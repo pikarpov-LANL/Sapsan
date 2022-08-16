191096fa8dd0f61b767fabe10b870047b991f60f
Platon I. Karpov <57731529+pikarpov-LANL@users.noreply.github.com>
2022-08-16 09:47:19 -0600

    Improvements to PIMLTurb estimator (#149)
    * Fixed SmoothL1_KSLoss train/valid output + set batch_num=1 by default
    
    * Scientific notation for PIMLturb loss stdout
    
    * Significant improvements to the PIMLturb estimator
    
    	- cleaned up redundancies improving the performance
    	  and readability
    	- generalized the approach to calculate CDF and KS loss
    		- now works with the 1D CCSN calculations
    		- should be consistent no matter the scale of the data
    	- added a few new parameters to be adjusted upon
               calling the estimator

--- sapsan/lib/estimator/pimlturb/pimlturb_diagonal_estimator.py
+++ sapsan/lib/estimator/pimlturb/pimlturb_diagonal_estimator.py
@@ -20,6 +20,7 @@ import torch
 from torch.autograd import Variable
 from torch.utils import data
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 from sapsan.core.models import EstimatorConfig
 from sapsan.lib.estimator.torch_backend import TorchBackend
@@ -84,121 +85,112 @@ class SmoothL1_KSLoss(torch.nn.Module):
     The loss functions combines a statistical (Kolmogorov-Smirnov)
     and a spatial loss (Smooth L1).
     
-    Corresponding 'losses_train_log.txt' and 'losses_valid_log.txt'
+    Corresponding 'train_l1ks_log.txt' and 'valid_l1ks_log.txt'
     are written out to include the individual loss evolutions.
     '''    
-    def __init__(self, ks_stop, scalel1, beta):
+    def __init__(self, ks_stop, ks_frac, ks_scale, l1_scale, beta, train_size, valid_size):
         super(SmoothL1_KSLoss, self).__init__()
-        self.counter = 1
-        self.first_write = 9
+        self.first_write = True
+        self.first_iter = True
         self.ks_stop = ks_stop
-        self.scalel1 = scalel1
+        self.ks_frac = ks_frac
+        self.ks_scale = ks_scale        
+        self.l1_scale = l1_scale
         self.beta = beta
-        self.stop = False        
+        self.train_size = train_size
+        self.valid_size = valid_size
+        self.stop = False   
         
-    def write_loss(self, losses, fname = "losses_train_log.txt"):
-        if self.counter==self.first_write:
-            if os.path.exists(fname): os.remove(fname)
+    def write_log(self, losses):
+        if self.first_write:
+            if os.path.exists(self.log_fname): os.remove(self.log_fname)
         
-        with open(fname,'a') as f:
-            if self.counter<=self.first_write: 
-                f.write("mean(L1_loss) \t mean(KS_loss) \t norm(L1_loss) \t norm(KS_loss) \t norm(L1_loss)*%.3e"%self.scaling)
+        with open(self.log_fname,'a') as f:
+            if self.first_write:
+                f.write(f"mean(L1_loss) \t mean(KS_loss) "\
+                        f"\t mean(L1_loss)*{self.l1_scale:.3e} \t mean(KS_loss)*{self.ks_scale:.3e}")
             f.write("\n")
             np.savetxt(f, losses.detach().cpu().numpy(), fmt='%.3e', newline="\t")
             
-    def write_valid(self, losses, fname = "losses_valid_log.txt"):
-        if self.counter==self.first_write+1:
-            if os.path.exists(fname): os.remove(fname)
-        
-        with open(fname,'a') as f:
-            if self.counter<=self.first_write+1: 
-                f.write("mean(L1_valid) \t mean(KS_valid)")
-            f.write("\n")
-            np.savetxt(f, losses.detach().cpu().numpy(), fmt='%.3e', newline="\t")
-        
-    def forward(self, predictions, targets):                   
+    def forward(self, predictions, targets, write, write_idx):      
         try: 
             self.device = predictions.get_device()
             if self.device==-1: self.device=torch.device('cpu')
-        except: self.device=torch.device('cpu')
-
-        size = list(targets.size())[0]  
-        lossks= torch.zeros(size).to(self.device)
+        except: self.device=torch.device('cpu')           
         
         #-----SmoothL1----
-        lossl1 = 0
+        l1_loss = 0
 
         self.beta = 0.1*targets.max()
         diff = predictions-targets
         mask = (diff.abs() < self.beta)
-        lossl1 += mask * (0.5*diff**2 / self.beta)
-        lossl1 += (~mask) * (diff.abs() - 0.5*self.beta)
+        l1_loss += mask * (0.5*diff**2 / self.beta)
+        l1_loss += (~mask) * (diff.abs() - 0.5*self.beta)
+           
+        #--------KS-------
+        distr = torch.distributions.normal.Normal(loc=targets.mean(), 
+                                                  scale=targets.std(), 
+                                                  validate_args=False)  
+        cdf,idx = distr.cdf(targets).flatten().to(self.device).sort()
         
-        for i in range(size):                        
-            #-------KS------
-            distr = torch.distributions.normal.Normal(loc=0, scale=1, validate_args=False)        
+        distr = torch.distributions.normal.Normal(loc=predictions.mean(), 
+                                                  scale=predictions.std(), 
+                                                  validate_args=False) 
+        cdf_pred,idx_pred = distr.cdf(predictions).flatten().to(self.device).sort()     
+                                
+        vals,idx = targets.flatten().to(self.device).sort()
+        vals_pred,idx_pred = predictions.flatten().to(self.device).sort()        
 
-            cdf = distr.cdf(targets[i]).to(self.device)
-            cdf_pred = distr.cdf(predictions[i]).to(self.device)
-
-            cdf, idx = cdf.flatten().to(self.device).sort()
-            cdf_pred, idx_pred = cdf_pred.flatten().to(self.device).sort()
-
-            y = torch.linspace(0,1,list(cdf.size())[0]).to(self.device)
-
-            y_new = Interp1d()(cdf, y, cdf_pred)[0]
-            
-            ks = torch.max(torch.abs(y-y_new)).to(self.device)         
-            
-            lossks[i] = ks
+        cdf_interp = Interp1d()(vals_pred, cdf_pred, vals)[0]       
         
-        if self.counter == 1:   
-            self.iter_losses_l1 = torch.zeros(self.first_write,requires_grad=False).to(self.device)
-            self.iter_losses_ks = torch.zeros(self.first_write,requires_grad=False).to(self.device)
-            
-            self.maxl1 = lossl1.mean().detach()                      
-            self.maxks = lossks.mean().detach()
+        ks_loss = torch.max(torch.abs(cdf-cdf_interp)).to(self.device)           
+        
+        #--Print Metrics-- 
+        if self.first_iter:
+            self.maxl1 = l1_loss.mean().detach()                      
+            self.maxks = ks_loss.mean().detach()
             self.max_ratio = self.maxks/self.maxl1
                         
-            print('---Max L1, KS losses and their ratio--')
+            print('---Initial (max) L1, KS losses and their ratio--')
             print(self.maxl1, self.maxks, self.max_ratio)
-            print('--------------------------------------')
+            print('------------------------------------------------')
             
-            self.scaling = self.maxl1*self.scalel1                                
+            self.first_iter = False
 
-        fracks = 0.5
-        fracl1 = 1 - fracks        
+        #----Calc Loss----
+        #fractions that KS and L1 contribute to the total loss        
+        l1_frac = 1-self.ks_frac
         
-        loss = (fracl1*lossl1.mean()/self.maxl1*self.scaling +
-                fracks*lossks.mean()/self.maxks)
-        #----------------
-        if (self.counter % 10) != 0: 
-            self.iter_losses_l1[(self.counter % 10)-1] = lossl1.mean().detach()
-            self.iter_losses_ks[(self.counter % 10)-1] = lossks.mean().detach()
-        #----------------     
+        loss = (l1_frac*l1_loss.mean()*self.l1_scale +
+                self.ks_frac*ks_loss.mean()*self.ks_scale)
         
-        if (self.counter % 10) == self.first_write:
+        #---Save Batch----
+        if write_idx == 0:
+            if write == 'train': 
+                self.batch_size = self.train_size
+                self.log_fname = "train_l1ks_log.txt"
+            elif write == 'valid': 
+                self.batch_size = self.valid_size 
+                self.log_fname = "valid_l1ks_log.txt"                
+            self.iter_losses_l1 = torch.zeros(self.batch_size,requires_grad=False).to(self.device)
+            self.iter_losses_ks = torch.zeros(self.batch_size,requires_grad=False).to(self.device)
+        
+        self.iter_losses_l1[write_idx] = l1_loss.mean().detach()
+        self.iter_losses_ks[write_idx] = ks_loss.mean().detach()
+        
+        #---Write Logs----
+        if write_idx == (self.batch_size-1):
             mean_iter_l1 = self.iter_losses_l1.mean()
             mean_iter_ks = self.iter_losses_ks.mean()
             
             if mean_iter_ks < self.ks_stop or self.stop==True:
                 self.stop = True
-                
-            mean_l1_valid = lossl1.mean()
-            mean_ks_valid = lossks.mean()
-            self.write_loss(torch.tensor([mean_iter_l1, mean_iter_ks,
-                                          mean_iter_l1/self.maxl1,
-                                          mean_iter_ks/self.maxks,
-                                          mean_iter_l1/self.maxl1*self.scaling]))      
-            self.iter_losses_l1.fill_(0)
-            self.iter_losses_ks.fill_(0) 
-                                                
-        if (self.counter % 10) == 0:
-            mean_l1_valid = lossl1.mean()
-            mean_ks_valid = lossks.mean()
-            self.write_valid(torch.tensor([mean_l1_valid, mean_ks_valid]))                  
+            
+            self.write_log(torch.tensor([mean_iter_l1, mean_iter_ks,
+                                         mean_iter_l1*self.l1_scale,
+                                         mean_iter_ks*self.ks_scale]))      
+            if write == 'valid': self.first_write = False                  
         
-        self.counter +=1           
         return loss, self.stop
     
 class PIMLTurbConfig(EstimatorConfig):
@@ -225,8 +217,10 @@ class PIMLTurb(TorchBackend):
     def __init__(self, activ, loss,
                        loaders,
                        ks_stop = 0.1,
-                       scalel1 = 1e6,
-                       betal1 = 1,
+                       ks_frac = 0.5,
+                       ks_scale = 1,
+                       l1_scale = 1,                 
+                       l1_beta = 1,
                        sigma = 1,
                        config = PIMLTurbConfig(), 
                        model = PIMLTurbModel()):
@@ -235,16 +229,23 @@ class PIMLTurb(TorchBackend):
         self.loaders = loaders
         self.device = self.config.kwargs['device']
         self.ks_stop = ks_stop
-        self.scalel1 = scalel1
-        self.beta = betal1
+        self.ks_frac = ks_frac
+        self.ks_scale = ks_scale
+        self.l1_scale = l1_scale        
+        self.beta = l1_beta
         
-        x_shape, y_shape = get_loader_shape(self.loaders)
-        
+        x_shape, y_shape = get_loader_shape(self.loaders)                
+
         self.model = PIMLTurbModel(x_shape[1], y_shape[2]**3, activ, sigma)
         
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.lr)        
         
-        if loss == "SmoothL1_KSLoss": self.loss_func = SmoothL1_KSLoss(self.ks_stop, self.scalel1, self.beta)
+        if loss == "SmoothL1_KSLoss": 
+            self.loss_func = SmoothL1_KSLoss(ks_stop = self.ks_stop, ks_frac = self.ks_frac,
+                                             ks_scale = self.ks_scale, l1_scale = self.l1_scale, 
+                                             beta = self.beta,
+                                             train_size = len(self.loaders['train']), 
+                                             valid_size = len(self.loaders['valid']))
         else: 
             print("This network only supports the custom SmoothL1_KSLoss. Please set the 'loss' parameter in config")
             sys.exit()
@@ -255,20 +256,15 @@ class PIMLTurb(TorchBackend):
         self.config.parameters["model - loss"] = str(self.loss_func).partition("(")[0]
         self.config.parameters["model - activ"] = activ
         
-    def write_loss(self, losses, epoch, fname = "train_log.txt", fname_valid='valid_log.txt'):
-        if epoch==1:
-            if os.path.exists(fname): os.remove(fname)
-            if os.path.exists(fname_valid): os.remove(fname_valid)
-        
-        with open(fname,'a') as f:
-            if epoch == 1: f.write("epoch \t train_loss")
-            f.write("\n")
-            np.savetxt(f, [[losses[0],losses[1]]], fmt='%d \t %.3e', newline="\t")
-            
-        with open(fname_valid,'a') as f:
-            if epoch == 1: f.write("epoch \t valid_loss")
-            f.write("\n")
-            np.savetxt(f, [[losses[0],losses[2]]], fmt='%d \t %.3e', newline="\t")
+    def write_loss(self, losses, epoch, fname_train = "train_log.txt", fname_valid='valid_log.txt'):
+        for idx, fname in enumerate([fname_train, fname_valid]):
+            if epoch==1:
+                if os.path.exists(fname): os.remove(fname)
+
+            with open(fname,'a') as f:
+                if epoch == 1: f.write("epoch \t loss")
+                f.write("\n")
+                np.savetxt(f, [[losses[0],losses[idx+1]]], fmt='%d \t %.3e', newline="\t")
         
     def train(self):        
         self.model.to(self.device)
@@ -282,8 +278,8 @@ class PIMLTurb(TorchBackend):
                 x = x.to(self.device)
                 y = y.to(self.device)
                 
-                y_pred = self.model(x) 
-                loss, stop = self.loss_func(y_pred, y)
+                y_pred = self.model(x)
+                loss, stop = self.loss_func(y_pred, y, 'train', idx)
                 
                 self.optimizer.zero_grad()
                 loss.backward()
@@ -292,7 +288,7 @@ class PIMLTurb(TorchBackend):
                 iter_loss[idx] = loss.item()
                         
             epoch_loss = iter_loss.mean()
-            print(f'train ({epoch}/{int(self.config.n_epochs)}) loss: {epoch_loss}')                        
+            print(f'train ({epoch}/{int(self.config.n_epochs)}) loss: {epoch_loss:.4e}')
                         
             with torch.set_grad_enabled(False):
                 iter_loss_valid = np.zeros(len(self.loaders['valid']))
@@ -302,12 +298,12 @@ class PIMLTurb(TorchBackend):
 
                     y_pred = self.model(x)                                    
                     
-                    loss_valid, stop = self.loss_func(y_pred, y)
-    
+                    loss_valid, stop = self.loss_func(y_pred, y, 'valid', idx)
+
                     iter_loss_valid[idx] = loss_valid.item()
 
                 epoch_loss_valid = iter_loss_valid.mean()
-                print(f'valid ({epoch}/{int(self.config.n_epochs)}) loss: {epoch_loss_valid}')
+                print(f'valid ({epoch}/{int(self.config.n_epochs)}) loss: {epoch_loss_valid:.4e}')
             
             self.write_loss([epoch, epoch_loss, epoch_loss_valid], epoch)            
             
@@ -315,6 +311,6 @@ class PIMLTurb(TorchBackend):
                 print('Reached sufficient KS Loss, stopping...')
                 break
                     
-            print('---')            
+            print('-----')            
                 
         return self.model
