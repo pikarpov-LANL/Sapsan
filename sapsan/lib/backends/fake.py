de662318230e457168f1294950aca5c8bfa2c23b
pikarpov-LANL <plkarpov@ucsc.edu>
2022-08-25 16:55:41 +0100

    Examples now include output, updated sample data for picae, fake backend
    	- cleaned up Examples to be up-to-date on comments
    	- returned FakeBackend

--- sapsan/lib/backends/fake.py
+++ sapsan/lib/backends/fake.py
@@ -4,23 +4,30 @@ from sapsan.core.models import ExperimentBackend
 
 
 class FakeBackend(ExperimentBackend):    
-    def start(self, run_name: str, nested: bool = False):
+    def start(self, run_name: str, nested: bool = False, run_id = None):
+        pass
+
+    def resume(self, run_id, nested = True):
+        pass
+    
+    def log_metric(self, name: str, value: float):
+        #logging.info("Logging experiment '{experiment}' metric "
+        #             "{name}: {value}".format(experiment=self.name,
+        #                                      name=name,
+        #                                      value=value))
         pass
     
     def log_parameter(self, name: str, value: str):
-        logging.info("Logging experiment '{experiment}' parameter "
-                     "{name}: {value}".format(experiment=self.name,
-                                              name=name,
-                                              value=value))
+        pass
+        #logging.info("Logging experiment '{experiment}' parameter "
+        #             "{name}: {value}".format(experiment=self.name,
+        #                                      name=name,
+        #                                      value=value))
 
     def log_artifact(self, path: str):
-        logging.info("Logging artifact {path}".format(path=path))
+        #logging.info("Logging artifact {path}".format(path=path))
+        pass
 
-    def log_metric(self, name: str, value: float):
-        logging.info("Logging experiment '{experiment}' metric "
-                     "{name}: {value}".format(experiment=self.name,
-                                              name=name,
-                                              value=value))
     def close_active_run(self):
         pass
     
